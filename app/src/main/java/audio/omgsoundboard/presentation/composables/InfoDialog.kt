533ef058dad466456c2e2e7623dbc2eb17662731
rash <mrtuxtrons@gmail.com>
2024-09-24 10:37:31 -0500

    fix

--- app/src/main/java/audio/omgsoundboard/presentation/composables/InfoDialog.kt
+++ app/src/main/java/audio/omgsoundboard/presentation/composables/InfoDialog.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import audio.omgsoundboard.R
+import audio.omgsoundboard.core.R
 
 @Composable
 fun InfoDialog(
