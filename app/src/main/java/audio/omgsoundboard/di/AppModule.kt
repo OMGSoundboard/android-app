83cc3b9dc63942d064c998110f8d4d924b5aac23
rash <mrtuxtrons@gmail.com>
2024-09-24 05:24:05 -0500

    Shared Preferences

--- app/src/main/java/audio/omgsoundboard/di/AppModule.kt
+++ app/src/main/java/audio/omgsoundboard/di/AppModule.kt
@@ -1,8 +1,8 @@
 package audio.omgsoundboard.di
 
 import android.content.Context
-import audio.omgsoundboard.data.UserPreferencesImpl
-import audio.omgsoundboard.domain.repository.UserPreferences
+import audio.omgsoundboard.data.StorageRepositoryImpl
+import audio.omgsoundboard.domain.repository.StorageRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,8 +16,8 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideUserPreferences(@ApplicationContext context: Context) : UserPreferences {
-        return UserPreferencesImpl(context)
+    fun providesStorage(@ApplicationContext context: Context) : StorageRepository {
+        return StorageRepositoryImpl(context)
     }
 
 }
